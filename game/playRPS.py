2ca549b2a624a89b2b6ccbb4514e4ad8290d2b11
HOON\hoonc <y88.choi@gmail.com>
2026-07-26 10:37:42 +0900

    기본골자 조정 및 main의 반복성과 숫자 선택 안할시 유저 초이스 다시하기 추가

--- game/playRPS.py
+++ game/playRPS.py
@@ -1,2 +1,33 @@
 def playRPS():
     print("==================== ROCK PAPER SCISSORS GAME ====================")
+
+    import random
+
+    answer = random.randint(1, 3)
+
+    if answer == 1:
+        answer = "가위"
+    if answer == 2:
+        answer = "바위"
+    if answer == 3:
+        answer = "보"
+
+    result = " "
+
+    while True:
+        hand = input("가위 바위 보:")
+        if hand != "가위" and hand != "바위" and hand != "보":
+            print("가위, 바위, 보 중에 다시 선택하세요")
+        elif hand == answer:
+            print(f"컴퓨터: {answer} 유저: {hand}")
+            print("다시 선택하세요")
+        elif answer != hand:
+            print(f"컴퓨터: {answer} 유저: {hand}")
+            if hand == "가위":
+                result = "컴퓨터승" if answer == "바위" else "유저승"
+            elif hand == "바위":
+                result = "컴퓨터승" if answer == "보" else "유저승"
+            elif hand == "보":
+                result = "컴퓨터승" if answer == "가위" else "유저승"
+            print(result)
+            break
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from game.playLotto import *
 
 while True:
     # 메뉴 출력
+    print("==================== Mini Games====================")
     print("1. 업다운 게임")
     print("2. 가위바위보")
     print("3. 로또 번호 맞추기")
-
+    print("0. 종료")
 
     # 선택 입력 받기
     menu = int(input("메뉴 선택 : "))
@@ -18,3 +19,9 @@ while True:
         playRPS()
     elif menu == 3:
         playLotto()
+
+    elif menu == 0:
+        print("==================== Mini Games Ended ====================")
+        break
+    else:
+        print("1, 2, 3, 0 중 하나를 선택하시오")
