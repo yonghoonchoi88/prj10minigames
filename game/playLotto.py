17d3672521e9fc95faf3805f243e7d212a102fcf
HOON\hoonc <y88.choi@gmail.com>
2026-07-26 13:41:19 +0900

    로또게임 추가

--- game/playLotto.py
+++ game/playLotto.py
@@ -1,2 +1,50 @@
+import random
+
 def playLotto():
     print("==================== Lotto Game ====================")
+
+    while True:
+        lotto_list = sorted(random.sample(range(1, 51), 6))
+        user_list = []
+        print("1부터 50까지중에 번호를 고르세요. (중복불가)")
+
+        for i in range(6):
+            while True:
+                number = int(input(f"{i + 1}번째 번호 : "))
+                if number in user_list:
+                    print("중복된 번호에요 다시 입력하세요.")
+                elif number > 50 or number < 1:
+                    print("1부터 50까지만 가능합니다.")
+                else:
+                    user_list.append(number)
+                    break
+        user_list = sorted(user_list)
+
+        count = 0
+        for i in lotto_list:
+            for j in user_list:
+                if i == j:
+                    count += 1
+
+        if count == 0:
+            result = "꽝"
+        elif count == 1:
+            result = "6등"
+        elif count == 2:
+            result = "5등"
+        elif count == 3:
+            result = "4등"
+        elif count == 4:
+            result = "3등"
+        elif count == 5:
+            result = "2등"
+        else:
+            result = "1등"
+
+        print(f"당첨번호 : {lotto_list}")
+        print(f"내 번호 : {user_list}")
+        print(f"{count}/6 번호 일치 -> {result}")
+
+        replay = input("Press any key to replay (N 입력시 종료)").upper()
+        if replay == "N":
+            break
