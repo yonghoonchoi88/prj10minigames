6ee60e42debe9df95a548f11cf62e4b2ae03025f
HOON\hoonc <y88.choi@gmail.com>
2026-07-26 11:10:50 +0900

    가위 바위보 업그레이드 후 제작 완료

--- game/playRPS.py
+++ game/playRPS.py
@@ -1,26 +1,19 @@
+import random
+
 def playRPS():
     print("==================== ROCK PAPER SCISSORS GAME ====================")
 
-    import random
-
-    answer = random.randint(1, 3)
-
-    if answer == 1:
-        answer = "가위"
-    if answer == 2:
-        answer = "바위"
-    if answer == 3:
-        answer = "보"
-
-    result = " "
-
     while True:
+        answer = random.randint(1, 3)
+        answer = "가위" if answer == 1 else "바위" if answer == 2 else "보"
+
         hand = input("가위 바위 보:")
         if hand != "가위" and hand != "바위" and hand != "보":
             print("가위, 바위, 보 중에 다시 선택하세요")
         elif hand == answer:
             print(f"컴퓨터: {answer} 유저: {hand}")
-            print("다시 선택하세요")
+            print("비겼습니다! 다시 선택하세요")
+
         elif answer != hand:
             print(f"컴퓨터: {answer} 유저: {hand}")
             if hand == "가위":
@@ -30,4 +23,6 @@ def playRPS():
             elif hand == "보":
                 result = "컴퓨터승" if answer == "가위" else "유저승"
             print(result)
+        replay = str(input("Press any key to replay (N 입력시 종료)").upper())
+        if replay == "N":
             break
